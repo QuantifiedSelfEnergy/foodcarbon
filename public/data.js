e33058d51b4be6d1e75572e598c23ea57662705a
Jorge Zapico <jorge@zapi.co>
2017-09-14 10:49:40 +0200

    Added pork

--- public/data.js
+++ public/data.js
@@ -1,25 +1,26 @@
 conversions = {
 	"beef" : {"id":1,"slug":"beef","unit":"kg of beef","base_value":1,"name":"Beef","description":"led","icon":"/images/units/led.jpg","category_id":2,"carbon":26.0},
 	"chicken" : {"id":2,"slug":"chicken","unit":"kg of chicken","base_value":1,"name":"Chicken","description":"led","icon":"/images/units/fridge.jpg","category_id":2,"carbon":3.0},
-	"egg" : {"id":3,"slug":"egg","unit":"dozens of eggs","base_value":1,"name":"Eggs","description":"led","icon":"/images/units/solarpanel.jpg","category_id":3,"carbon":1.125},
-	"beans" : {"id":4,"slug":"beans","unit":"kg of beans","base_value":1,"name":"Beans","description":"led","icon":"/images/units/car.jpg","category_id":3,"carbon":0.7},
-	"quorn" : {"id":5,"slug":"quorn","unit":"kg of quorn","base_value":1,"name":"Quorn","description":"led","icon":"/images/units/kwh.jpg","category_id":1,"carbon":4.0},
-	"fish" : {"id":6,"slug":"fish","unit":"kg of fish","base_value":1,"name":"Fish","description":"led","icon":"/images/units/solarpanel.jpg","category_id":1,"carbon":3.0},
-	"milk" : {"id":7,"slug":"milk","unit":"liters of milk or yogurt","base_value":1,"name":"Milk","description":null,"icon":"/images/units/lightbulb.jpg","category_id":2,"carbon":1.0},
-	"cheese" : {"id":9,"slug":"cheese","unit":"kg of cheese or butter","base_value":1,"name":"Cheese","description":null,"icon":"/images/units/tv.jpg","category_id":2,"carbon":8.0},
-	"potatoes" : {"id":10,"slug":"potatoes","unit":"kg of potatoes","base_value":1,"name":"Potatoes","description":"wifi","icon":"/images/units/router.jpg","category_id":2,"carbon":0.1},
-	"rice" : {"id":11,"slug":"rice","unit":"kg of rice","base_value":1,"name":"Rice","description":null,"icon":"/images/units/heating.jpg","category_id":4,"carbon":2.0},
-	"bread" : {"id":12,"slug":"bread","unit":"kg of bread or pasta","base_value":1,"name":"Bread","description":null,"icon":"/images/units/heating.jpg","category_id":4,"carbon":0.8},
-	"fruit" : {"id":13,"slug":"fruit","unit":"kg of fruit (domestic)","base_value":1,"name":"Fruit","description":null,"icon":"/images/units/coal.jpg","category_id":1,"carbon":0.2},
-	"vegetables" : {"id":14,"slug":"vegetables","unit":"kg of sallad vegetables (domestic)","base_value":1,"name":"Vegetables","description":null,"icon":"/images/units/treadmill.jpg","category_id":1,"carbon":1.0},
-	"airfreight" : {"id":15,"slug":"airfreight","unit":"kg of airfreighted fruit or vegetables","base_value":1,"name":"Airfreighted fruit","description":null,"icon":"/images/units/treadmill.jpg","category_id":1,"carbon":11.0},
-	"coffee" : {"id":16,"slug":"coffee","unit":"kg of coffee beans","base_value":1,"name":"Coffee","description":null,"icon":"/images/units/treadmill.jpg","category_id":1,"carbon":3.0},
+	"pork" : {"id":3,"slug":"pork","unit":"kg of pork","base_value":1,"name":"Pork","description":"led","icon":"/images/units/fridge.jpg","category_id":2,"carbon":6.0},
+	"egg" : {"id":5,"slug":"egg","unit":"dozens of eggs","base_value":1,"name":"Eggs","description":"led","icon":"/images/units/solarpanel.jpg","category_id":3,"carbon":1.125},
+	"beans" : {"id":6,"slug":"beans","unit":"kg of beans","base_value":1,"name":"Beans","description":"led","icon":"/images/units/car.jpg","category_id":3,"carbon":0.7},
+	"quorn" : {"id":7,"slug":"quorn","unit":"kg of quorn","base_value":1,"name":"Quorn","description":"led","icon":"/images/units/kwh.jpg","category_id":1,"carbon":4.0},
+	"fish" : {"id":8,"slug":"fish","unit":"kg of fish","base_value":1,"name":"Fish","description":"led","icon":"/images/units/solarpanel.jpg","category_id":1,"carbon":3.0},
+	"milk" : {"id":9,"slug":"milk","unit":"liters of milk or yogurt","base_value":1,"name":"Milk","description":null,"icon":"/images/units/lightbulb.jpg","category_id":2,"carbon":1.0},
+	"cheese" : {"id":10,"slug":"cheese","unit":"kg of cheese or butter","base_value":1,"name":"Cheese","description":null,"icon":"/images/units/tv.jpg","category_id":2,"carbon":8.0},
+	"potatoes" : {"id":11,"slug":"potatoes","unit":"kg of potatoes","base_value":1,"name":"Potatoes","description":"wifi","icon":"/images/units/router.jpg","category_id":2,"carbon":0.1},
+	"rice" : {"id":12,"slug":"rice","unit":"kg of rice","base_value":1,"name":"Rice","description":null,"icon":"/images/units/heating.jpg","category_id":4,"carbon":2.0},
+	"bread" : {"id":13,"slug":"bread","unit":"kg of bread or pasta","base_value":1,"name":"Bread","description":null,"icon":"/images/units/heating.jpg","category_id":4,"carbon":0.8},
+	"fruit" : {"id":14,"slug":"fruit","unit":"kg of fruit (domestic)","base_value":1,"name":"Fruit","description":null,"icon":"/images/units/coal.jpg","category_id":1,"carbon":0.2},
+	"vegetables" : {"id":15,"slug":"vegetables","unit":"kg of sallad vegetables (domestic)","base_value":1,"name":"Vegetables","description":null,"icon":"/images/units/treadmill.jpg","category_id":1,"carbon":1.0},
+	"airfreight" : {"id":16,"slug":"airfreight","unit":"kg of airfreighted fruit or vegetables","base_value":1,"name":"Airfreighted fruit","description":null,"icon":"/images/units/treadmill.jpg","category_id":1,"carbon":11.0},
+	"coffee" : {"id":4,"slug":"coffee","unit":"kg of coffee beans","base_value":1,"name":"Coffee","description":null,"icon":"/images/units/treadmill.jpg","category_id":1,"carbon":3.0},
 	"co2": {"id":17,"slug":"co2","unit":"kg of CO2","base_value":1,"name":"CO2","description":null,"icon":"/images/units/treadmill.jpg","category_id":1,"carbon":1.0}
 	
 };
 
 index = [
-  "beef" , "chicken" ,  "egg" , "beans" , "quorn", "fish" , "milk" , "cheese" , "potatoes", "rice" , "bread", "fruit", "vegetables", "airfreight", "coffee", "co2"
+  "beef" , "chicken" ,  "pork" , "egg" , "beans" , "quorn", "fish" , "milk" , "cheese" , "potatoes", "rice" , "bread", "fruit", "vegetables", "airfreight", "coffee", "co2"
 ];
 
 
